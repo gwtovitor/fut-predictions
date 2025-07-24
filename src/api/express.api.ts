ccb86eaedeb5147bf6ad084b390e105820c27530
Vitor Augusto <gwtovitorpw@gmail.com>
2025-07-24 20:44:02 -0300

    adding cors

--- src/api/express.api.ts
+++ src/api/express.api.ts
@@ -1,12 +1,18 @@
 import { Api } from './api';
 import express, { Express } from 'express';
 import { Route } from './routes/route';
-
+import cors from 'cors';
 export class ApiExpress implements Api {
 	private app: Express;
 
 	private constructor(routes: Route[]) {
 		this.app = express();
+		this.app.use(
+			cors({
+				origin: 'http://localhost:5173', 
+				credentials: true,
+			})
+		);
 		this.app.use(express.json());
 		this.addRoutes(routes);
 	}
@@ -16,7 +22,6 @@ export class ApiExpress implements Api {
 	}
 
 	private addRoutes(routes: Route[]) {
-
 		routes.forEach((route) => {
 			const path = route.getPath();
 			const method = route.getMethod();
